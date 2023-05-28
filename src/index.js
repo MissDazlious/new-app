abc134b6652bf80017e423735ee24fbee5cdf213
MissDazlious <129790484+MissDazlious@users.noreply.github.com>
2023-05-28 19:53:36 +0100

    Added files

---
+++ src/index.js
@@ -0,0 +1,56 @@
+function citySubmit(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#city");
+  let apiKey = "a2dda52dce059eb8a14e95aaa0db6ab7";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&units=metric`;
+  axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemperature);
+}
+
+function showTemperature(response) {
+  console.log(response.data);
+  let mainCity = document.querySelector("#maincity");
+  mainCity.innerHTML = response.data.name;
+  let mainTemp = document.querySelector("#main-temp");
+  mainTemp.innerHTML = `${Math.round(response.data.main.temp)}°`;
+  let minTemp = document.querySelector("#min-temp");
+  minTemp.innerHTML = `Min: ${Math.round(response.data.main.temp_min)}°`;
+  let maxTemp = document.querySelector("#max-temp");
+  maxTemp.innerHTML = `Max: ${Math.round(response.data.main.temp_max)}°`;
+  let wind = document.querySelector("#wind");
+  wind.innerHTML = `Wind: ${Math.round(response.data.wind.speed)} km/h`;
+  let humidity = document.querySelector("#humidity");
+  humidity.innerHTML = `Humidity: ${response.data.main.humidity}%`;
+}
+
+let form = document.querySelector("#city-form");
+form.addEventListener("submit", citySubmit);
+
+function showPosition(position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric`;
+  let apiKey = "a2dda52dce059eb8a14e95aaa0db6ab7";
+  axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemperature);
+}
+
+function currentPosition(position) {
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+let currentButton = document.querySelector("#current");
+currentButton.addEventListener("click", currentPosition);
+
+let now = new Date();
+let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+let day = days[now.getDay()];
+let hour = now.getHours();
+if (hour < 10) {
+  hour = `0${hour}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
+let currentDate = document.querySelector("#date");
+currentDate.innerHTML = `${day} ${hour}:${minutes}`;
